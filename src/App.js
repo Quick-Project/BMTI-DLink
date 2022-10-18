809a2d499e1fc4293c1f67414214a4937abf9921
haileyport <sugar9935@gmail.com>
2022-10-18 15:47:32 +0900

    🍸 feat : router 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import SocialShare from './components/SocialShare';
-import ProgressBar from './components/ProgressBar';
-import Graph from './components/Graph';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Home from './pages/Home';
+
+const WebPage = styled.article`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background: linear-gradient
+    (180deg, ${(props) => props.one} 0%, ${(props) => props.two} 100%);
+`;
+
+const MobileFit = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+
+  //차후 지울 보더
+  border: 1px solid red;
+
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
 
 function App() {
-  const hi = 'hi';
+  // 그라디언트 state
+  const [one, setOne] = useState('white');
+  const [two, setTwo] = useState('#FAF6EE');
+
   return (
-    <div>
-      <span>
-        {/* <SocialShare
-          _title={'DEEP'}
-          _sub={'파우스트'}
-          _imageUrl={
-            'https://cloud-mustang-79a.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0496b621-8066-4bf1-bfea-59133e7b706d%2FUntitled.png?table=block&id=05212b9f-aaaf-41bb-9679-32f863f0b01c&width=2000&userId=&cache=v2'
-          }
-        /> */}
-      </span>
-    </div>
+    <BrowserRouter>
+      <WebPage one={one} two={two}>
+        <MobileFit>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </MobileFit>
+      </WebPage>
+    </BrowserRouter>
   );
 }
 export default App;
