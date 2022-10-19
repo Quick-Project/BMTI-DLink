aa9a3092393f59d0e6690bb5ad60e805c8fbb7ec
billy5982 <kuu0404@naver.com>
2022-10-19 23:23:51 +0900

    🍻 refactor : ShareUrl 변경

--- src/pages/ResultScreen.js
+++ src/pages/ResultScreen.js
@@ -186,7 +186,7 @@ const ResultScreen = ({ cocktailType, BMTIdata, setCocktailType }) => {
           _sub={`[ ${filter[0].type} ]`}
           _imageUrl={filter[0].cocktailImg}
           setCocktailType={setCocktailType}
-          _url={process.env.REACT_APP_SHARE_URL}
+          _url={process.env.PUBLIC_URL}
         />
       </ShareContents>
 
@@ -240,7 +240,7 @@ const ResultScreen = ({ cocktailType, BMTIdata, setCocktailType }) => {
           _sub={`[ ${filter[0].type} ]`}
           _imageUrl={filter[0].cocktailImg}
           setCocktailType={setCocktailType}
-          _url={process.env.REACT_APP_SHARE_URL}
+          _url={process.env.PUBLIC_URL}
         />
       </ShareContents>
     </ResultContain>
